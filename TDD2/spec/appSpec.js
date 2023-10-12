03857e220db6c2eb0478a807b96f79870e283f6f
imane-temsamani-vinci <temsamani.imane@student.vinci.be>
2023-10-12 18:02:06 +0200

    berlin clock

---
+++ TDD2/spec/appSpec.js
@@ -0,0 +1,14 @@
+import { BerlinClock } from "../src/app.js";
+
+describe("the first minute line", function () {
+  it("minuteLine given ... should return ...", function () {
+    //arrange
+    let time = ;
+
+    //act
+    let result = BerlinClock.minuteLine(time);
+
+    //assert
+    expect(result).toBe("...");
+  });
+});
